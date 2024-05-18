a39c39ea4ca61eaa9dd9fcb86b8c93c94d17ec0d
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2024-05-18 13:34:57 -0600

    Work on input_processing

--- input_processing.py
+++ input_processing.py
@@ -1,5 +1,5 @@
 # input_processing.py
-# YOUR NAME, ENSF 692 P24
+# JEFF WHEELER, ENSF 692 P24
 # A terminal-based program for processing computer vision changes detected by a car.
 # Detailed specifications are provided via the Assignment 2 README file.
 # You must include the code provided below but you may delete the instructional comments.
@@ -12,25 +12,44 @@
 # No global variables are permitted
 
 
-# You do not need to provide additional commenting above this class, just the user-defined functions within the class
+
 class Sensor:
 
-    # Must include a constructor that uses default values
-    # You do not need to provide commenting above the constructor
     def __init__(self):
         self.light = "green"
         self.pedestrian = "no"
         self.vehicle = "no"
 
-    # Replace these comments with your function commenting
-    def update_status(): # You may decide how to implement the arguments for this function
-        pass
+    # This function takes a string input and updates the Sensor instance variables based on further input
+    def update_status(self, n):
+        if n == "1":
+            x = input("What change has been identified?: ")
+            if x == "red" or x == "green" or x == "yellow":
+                self.light = x
+            else: 
+                print("Invalid vision change.\n")
+        elif n == "2":
+            y = input("What change has been identified?: ")
+            if y == "yes" or y == "no":
+                self.pedestrian = y
+            else:
+                print("Invalid vision change.\n")          
+        elif n == "3":
+            z = input("What change has been identified?: ")
+            if z == "yes" or z == "no":
+                self.vehicle = z
+            else:
+                print("Invalid vision change")
+        elif n == "0":
+            print("Program Ended\n")
+        else:
+            print("Input must be 1, 2, 3 or 0")
 
 
 
 
-# The sensor object should be passed to this function to print the action message and current status
-# Replace these comments with your function commenting
+
+# This function takes a Sensor object and prints 
 def print_message(Sensor):
     if Sensor.light == "red" or Sensor.pedestrian == "yes" or Sensor.vehicle == "yes":
         print("\nSTOP\n")
@@ -47,44 +66,20 @@ def main():
     print("\n***ENSF 692 Car Vision Detector Processing Program***\n")
     
     sensor = Sensor()
-    while 1 == 1:
+    n = ""
+    while n != "0":
         print("Are changes detected in the vision input?")
-        n = input("Select 1 for light, 2 for pedestrian, 3 for vehicle, or 0 to end program: ") 
-        if n == '1':
-            x = input("What change has been identified?: ")
-            if x == "red" or x == "green" or x == "yellow":
-                sensor.light = x
-                print_message(sensor)
-            else: 
-                print("Invalid vision change.\n")
-                print_message(sensor)
-        elif n == '2':
-            y = input("What change has been identified?: ")
-            if y == "yes" or y == "no":
-                sensor.pedestrian = y
-                print_message(sensor)
+        L = ['0', '1', '2', '3']
+        n = input("Select 1 for light, 2 for pedestrian, 3 for vehicle, or 0 to end program: ")
+        try:
+            if n in L:
+                sensor.update_status(n)
             else:
-                print("Invalid vision change.\n")
-                print_message(sensor)            
-        elif n == '3':
-            z = input("What change has been identified?: ")
-            if z == "yes" or z == "no":
-                sensor.vehicle = z
-                print_message(sensor)
-            else:
-                print("Invalid vision change")
-                print_message(sensor)
-        elif n == '0':
-            print("Program Ended\n")
-            break
-        else:
-            print("You must select either 1, 2, 3 or 0.\n")  
-    
-    
-    
-            
-
-
+                raise ValueError
+        except ValueError:
+            print("You must select either 1, 2, 3 or 0.\n")
+        if n != "0":
+            print_message(sensor)
 
 
 # Conventional Python code for running main within a larger program
